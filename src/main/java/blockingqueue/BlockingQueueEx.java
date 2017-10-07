342ebdcbbc0790cb4680338604505f9f18386cdf
xin wei <xin_wei@intuit.com>
2017-10-07 06:10:07 -0700

    blocking queue example

--- src/main/java/blockingqueue/BlockingQueueEx.java
+++ src/main/java/blockingqueue/BlockingQueueEx.java
@@ -1,4 +1,19 @@
 package blockingqueue;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class BlockingQueueEx {
+    public static void main(String[] args) throws Exception {
+
+        BlockingQueue queue = new ArrayBlockingQueue(1024);
+
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
+        new Thread(producer).start();
+        new Thread(consumer).start();
+
+        Thread.sleep(4000);
+    }
 }
